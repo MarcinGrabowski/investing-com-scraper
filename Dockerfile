87eeba1e1d0e03082a7f2002c7c7d6578084760b
Marcin Grabowski <marcin.grabowski@modostudio.pl>
2022-12-09 22:47:42 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,36 @@
+FROM node:18-buster
+
+RUN apt-get update \
+    && apt-get upgrade -y \
+    && apt-get install -y wget gnupg \
+    && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
+    && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
+    && apt-get update \
+    && apt-get install -y vim htop google-chrome-stable fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst fonts-freefont-ttf libxss1 \
+      --no-install-recommends \
+    && rm -rf /var/lib/apt/lists/*
+
+RUN apt-get update -y && apt-get upgrade -y && apt-get install dumb-init -y
+
+RUN mkdir -p /usr/src/app
+RUN mkdir -p /usr/src/app/node_modules
+RUN mkdir -p /usr/src/app/data
+WORKDIR /usr/src/app
+COPY package.json /usr/src/app/
+RUN chmod -R 777 /usr/src/app
+RUN npm install
+RUN npm install -g npm@9.1.3
+RUN groupadd -r pptruser && useradd -r -g pptruser -G audio,video pptruser \
+    && mkdir -p /home/pptruser/Downloads \
+    && chown -R pptruser:pptruser /home/pptruser \
+    && chown -R pptruser:pptruser /usr/src/app \
+    && chown -R pptruser:pptruser /usr/src/app/node_modules
+
+USER pptruser
+
+COPY index.js /usr/src/app/
+
+EXPOSE 3000
+
+ENTRYPOINT ["/usr/bin/dumb-init", "--"]
+CMD [ "npm", "start" ]
---
+++ index.js
@@ -0,0 +1,56 @@
+const puppeteer = require("puppeteer")
+const fs = require("fs")
+const request = require('request')
+
+const scraping = async () => {
+    var startTime = performance.now();
+    console.log('RUN scraping ' + new Date().toLocaleString());
+    puppeteer.launch({
+        headless: true,
+        args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+        ]
+    }).then(async browser => {
+        const page = await browser.newPage()
+        await page.setViewport({
+            width: 650,
+            height: 650,
+        });
+
+        await page.goto('https://sslecal2.investing.com/?columns=exc_flags,exc_currency,exc_importance,exc_actual,exc_forecast,exc_previous&importance=3&countries=25,32,37,22,26,39,14,48,35,6,7,17,43,53,110,36,5,72,12,61,93,4,10&calType=day&timeZone=57&lang=15', 
+        {timeout: 5000, waitUntil: 'networkidle2'});
+        await page.waitForSelector('div.inlineblock');
+        const element = await page.$('div.inlineblock');
+        await element.screenshot({path: 'events.png'});
+
+        await page.waitForTimeout(9000);
+        publish()
+        await page.waitForTimeout(9000);
+        await browser.close()
+        process.exit()
+    }).catch((e) => {
+        console.log("[ERROR PUPPETEER]: ", e);
+        scraping();
+    });
+}
+
+scraping()
+
+async function publish() {
+    var headersOpt = {  
+        "Content-Type": "multipart/form-data"
+    };
+
+    var readStream = fs.createReadStream(__dirname + "/events.png");
+    await request(
+        {
+        method:'post',
+        url:'https://discord.com/api/webhooks/xxx', 
+        formData: { 'username': 'Investing.com', 'image': readStream },
+        headers: headersOpt,
+        json: true,
+    }, function (error, response, body) {  
+        console.log(error); 
+    });
+}
