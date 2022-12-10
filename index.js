cdfb7c4bbd4818db12e9bdffee19fb715d38f024
Marcin Grabowski <marcin.grabowski@modostudio.pl>
2022-12-10 11:08:00 +0100

    Zmiany

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const scraping = async () => {
     var startTime = performance.now();
     console.log('RUN scraping ' + new Date().toLocaleString());
     puppeteer.launch({
-        headless: true,
+        headless: false,
         args: [
           '--no-sandbox',
           '--disable-setuid-sandbox',
@@ -20,13 +20,20 @@ const scraping = async () => {
 
         await page.goto('https://sslecal2.investing.com/?columns=exc_flags,exc_currency,exc_importance,exc_actual,exc_forecast,exc_previous&importance=3&countries=25,32,37,22,26,39,14,48,35,6,7,17,43,53,110,36,5,72,12,61,93,4,10&calType=day&timeZone=57&lang=15', 
         {timeout: 5000, waitUntil: 'networkidle2'});
+        await page.waitForTimeout(3000);
+        await page.evaluate( function(){
+            setTimeFrame("timeFrame_today","today", false);
+        });
+        await page.waitForTimeout(3000);
+
         await page.waitForSelector('div.inlineblock');
         const element = await page.$('div.inlineblock');
+
         await element.screenshot({path: 'events.png'});
 
-        await page.waitForTimeout(9000);
+        await page.waitForTimeout(3000);
         publish()
-        await page.waitForTimeout(9000);
+        await page.waitForTimeout(3000);
         await browser.close()
         process.exit()
     }).catch((e) => {
@@ -46,7 +53,7 @@ async function publish() {
     await request(
         {
         method:'post',
-        url:'https://discord.com/api/webhooks/xxx', 
+        url:'https://discord.com/api/webhooks/' + process.env.DC_ADDRESS, 
         formData: { 'username': 'Investing.com', 'image': readStream },
         headers: headersOpt,
         json: true,
